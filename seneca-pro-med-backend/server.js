67c225edb7e419bbf728fdb2a6982f87199b264e
I68439663 <abajin@myseneca.ca>
2023-01-26 18:27:37 -0500

    Update server.js

--- seneca-pro-med-backend/server.js
+++ seneca-pro-med-backend/server.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 //Routes 
 app.use("/client", clientControllers);
 app.use("/admin", adminControllers);
-app.use("/driver",driverControllers);
+app.use("/driver", driverControllers);
 app.use("/pharmacy", pharmaControllers);
 
 
