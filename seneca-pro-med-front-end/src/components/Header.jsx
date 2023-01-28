791697b2e2e86de38a13f5b761b1d6bbd6f2358f
Zane Chan <chanzhenya@gmail.com>
2023-01-28 15:08:43 -0500

    Added menu icons to the header

--- seneca-pro-med-front-end/src/components/Header.jsx
+++ seneca-pro-med-front-end/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../assets/images/senecaProMed.png";
+import { MdShoppingCart, MdNotificationsNone } from "react-icons/md";
 
 const Header = () => {
   return (
@@ -8,13 +9,22 @@ const Header = () => {
       <div className="hidden md:flex w-full h-full">
         <div className="flex items-center gap-2">
           <img src={Logo} className="w-20 object-cover" alt="logo" />
-          <p className="text-headingColor text-xl font-bold"></p>
+          <p className="text-headingColor text-xl font-bold">Seneca Pro Med</p>
         </div>
+
         <ul className="flex items-center gap-8 ml-auto">
-          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Home</li>
-          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Cart</li>
-          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Notifications</li>
-          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">Profile</li>
+          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Home
+          </li>
+          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <MdShoppingCart size={30} />
+          </li>
+          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <MdNotificationsNone size={30} />
+          </li>
+          <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            My Account
+          </li>
         </ul>
       </div>
 
