b86c6abf68cfcbff8ecb5148c62b70580a36d34b
Zane Chan <chanzhenya@gmail.com>
2023-01-27 21:06:02 -0500

    Added router for navigation between pages

--- seneca-pro-med-front-end/src/index.js
+++ seneca-pro-med-front-end/src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "../src/assets/css/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
