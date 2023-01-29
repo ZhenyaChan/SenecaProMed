fa44131343bb838c0c493c419788cbb2c9dc9526
kevin <kpsanpedro@myseneca.ca>
2023-01-29 17:22:45 -0500

    Feat: update logo

--- seneca-pro-med-front-end/src/components/Header.jsx
+++ seneca-pro-med-front-end/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../assets/images/senecaProMed.png";
+import Logo from "../assets/images/SenecaProMed-Logo.svg";
 import {
   MdShoppingCart,
   MdNotificationsNone,
