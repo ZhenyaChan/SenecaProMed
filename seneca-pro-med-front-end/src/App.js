9eb4f2b3d6d00e9b17002e681080bd040c5e44a1
Zane Chan <chanzhenya@gmail.com>
2023-02-02 16:04:50 -0500

    Added routes to the App file and animate presence for smoother animation

--- seneca-pro-med-front-end/src/App.js
+++ seneca-pro-med-front-end/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import { Header } from "./components";
 
 const App = () => {
   return (
-    <div className="w-screen h-auto flex flex-col bg-primary">
-      <Header />
-      <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+    <AnimatePresence>
+      <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
 
-      </main>
-    </div>
+        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+          <Routes>
+            <Route path="/*" element="Main" />
+            <Route path="/*" element="" />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 };
 
