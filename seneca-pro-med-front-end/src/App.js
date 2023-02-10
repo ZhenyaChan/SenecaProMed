b1f8cc3ccdc37b69da6de0c67b5a283e73d30b5f
Zane Chan <chanzhenya@gmail.com>
2023-02-10 14:25:29 -0500

    Added implementation for product cards, added initial files for product list

--- seneca-pro-med-front-end/src/App.js
+++ seneca-pro-med-front-end/src/App.js
@@ -5,14 +5,22 @@ import { AnimatePresence } from "framer-motion";
 import HeaderComponent from "./components/HeaderComponent";
 import MainComponent from "./components/MainComponent";
 import FooterComponent from "./components/FooterComponent";
+import ProductComponent from "./components/Product";
+
 
 const App = () => {
   return (
     <AnimatePresence>
-      <div className="w-screen h- auto flex flex-col bg-primary">
+      <div className="w-screen h-auto flex flex-col bg-primary">
         <HeaderComponent />
 
         <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+          <ProductComponent title="Product 1" description="Blah blah" price="25"/>
+          <ProductComponent title="Product 2" description="Blah blah" price="25"/>
+          <ProductComponent title="Product 3" description="Blah blah" price="25"/>
+          <ProductComponent title="Product 4" description="Blah blah" price="25"/>
+          <ProductComponent title="Product 5" description="Blah blah" price="25"/>
+
           <Routes>
             <Route path="/*" element={<MainComponent />} />
           </Routes>
