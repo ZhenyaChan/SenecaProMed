a276e61d9d1ee0a18be398a0f6c5e223d6adfc03
I68439663 <abajin@myseneca.ca>
2023-02-12 23:27:38 -0500

    feat: working on create form

--- seneca-pro-med-front-end/src/App.js
+++ seneca-pro-med-front-end/src/App.js
@@ -5,8 +5,9 @@ import { AnimatePresence } from "framer-motion";
 import HeaderComponent from "./components/HeaderComponent";
 import MainComponent from "./components/MainComponent";
 import FooterComponent from "./components/FooterComponent";
-import PharmacyUsers from "./components/PharmacyUsers";
-import Pharmacy from "./components/Pharmacy";
+import PharmacyUsers from "./components/pharmacy/PharmacyUsers";
+import Pharmacy from "./components/pharmacy/Pharmacy";
+import CreatePharmacy from "./components/pharmacy/CreatePharmacy";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
             <Route path="/*" element={<MainComponent />} />
             <Route path="/pharmacyUsers" element={<PharmacyUsers />} />
             <Route path="/pharmacy/:id" element={<Pharmacy />} />
+            <Route path="/createPharmacy" element={<CreatePharmacy />} />
           </Routes>
         </main>
 
--- seneca-pro-med-front-end/src/components/pharmacy/Pharmacy.jsx
+++ seneca-pro-med-front-end/src/components/pharmacy/Pharmacy.jsx
@@ -71,7 +71,19 @@ export default function Pharmacy() {
                      <div className="flex justify-center">
                         <button
                            type="button"
-                           className=" inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                           className=" inline-block 
+                           px-6 py-2.5 
+                           bg-blue-600 
+                           text-white 
+                           font-medium text-xs 
+                           leading-tight 
+                           uppercase 
+                           rounded 
+                           shadow-md 
+                           hover:bg-blue-700 hover:shadow-lg 
+                           focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 
+                           active:bg-blue-800 active:shadow-lg 
+                           transition duration-150 ease-in-out"
                            onClick={() => {
                               navigate(`/pharmacyUsers`);
                            }}
@@ -81,7 +93,20 @@ export default function Pharmacy() {
                         &nbsp;&nbsp;&nbsp;
                         <button
                            type="button"
-                           className=" inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                           className=" inline-block 
+                           px-6 py-2.5 
+                           bg-green-500 
+                           text-white 
+                           font-medium 
+                           text-xs 
+                           leading-tight 
+                           uppercase 
+                           rounded 
+                           shadow-md 
+                           hover:bg-green-700 hover:shadow-lg 
+                           focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 
+                           active:bg-green-800 active:shadow-lg 
+                           transition duration-150 ease-in-out"
                            onClick={() => {
                               navigate(`/pharmacyUsers`);
                            }}
@@ -91,7 +116,21 @@ export default function Pharmacy() {
                         &nbsp;&nbsp;&nbsp;
                         <button
                            type="button"
-                           className=" inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                           className=" inline-block 
+                           px-6 
+                           py-2.5 
+                           bg-red-600 
+                           text-white 
+                           font-medium 
+                           text-xs 
+                           leading-tight 
+                           uppercase 
+                           rounded 
+                           shadow-md 
+                           hover:bg-red-700 hover:shadow-lg 
+                           focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 
+                           active:bg-red-800 active:shadow-lg 
+                           transition duration-150 ease-in-out"
                            onClick={() => {
                               navigate(`/pharmacyUsers`);
                            }}
