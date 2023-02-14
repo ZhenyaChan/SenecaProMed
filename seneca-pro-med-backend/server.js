eed40afa319042ad3595af6fda861acd45eeff7b
Humam Bahoo <humam.bahoo@gmail.com>
2023-02-14 13:20:49 -0500

    add cors() middleware to allow post request across origins

--- seneca-pro-med-backend/server.js
+++ seneca-pro-med-backend/server.js
@@ -1,42 +1,50 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const clientControllers = require("./controllers/client-controller.js");
-const adminControllers = require("./controllers/admin-controller.js");
-const driverControllers = require("./controllers/driver-controller.js");
-const pharmaControllers = require("./controllers/pharma-controller.js");
-const productControllers = require("./controllers/product-controller.js");
+const cors = require('cors');
+
+const clientControllers = require('./controllers/client-controller.js');
+const adminControllers = require('./controllers/admin-controller.js');
+const driverControllers = require('./controllers/driver-controller.js');
+const pharmaControllers = require('./controllers/pharma-controller.js');
+const productControllers = require('./controllers/product-controller.js');
 
 const PORT = 8080;
 const app = express();
+
+// using cors() middleware to make post requests across origins
+app.use(cors());
+
 app.use(express.json());
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
 //Home
-app.get("/", (req, res) => {
-    res.json({message: "Server started"});
+app.get('/', (req, res) => {
+  res.json({ message: 'Server started' });
 });
 
-//Routes 
-app.use("/client", clientControllers);
-app.use("/admin", adminControllers);
-app.use("/driver", driverControllers);
-app.use("/pharmacy", pharmaControllers);
-app.use("/product", productControllers)
-
+//Routes
+app.use('/client', clientControllers);
+app.use('/admin', adminControllers);
+app.use('/driver', driverControllers);
+app.use('/pharmacy', pharmaControllers);
+app.use('/product', productControllers);
 
 app.listen(PORT, () => {
-    console.log(`REST API is up and running on PORT: ${PORT}`);
+  console.log(`REST API is up and running on PORT: ${PORT}`);
 
-    mongoose.connect(`mongodb+srv://senecapromed:senec%40ProMed1234@senecapromeddb.xjhswji.mongodb.net/UsersDB?retryWrites=true&w=majority`)
+  mongoose
+    .connect(
+      `mongodb+srv://senecapromed:senec%40ProMed1234@senecapromeddb.xjhswji.mongodb.net/UsersDB?retryWrites=true&w=majority`
+    )
     .then(() => {
-        console.log(`Connected to the MongoDB`);
+      console.log(`Connected to the MongoDB`);
     })
-    .catch(err => {
-        console.log(`Error: ${err}`);
+    .catch((err) => {
+      console.log(`Error: ${err}`);
     });
 });
 
-//HTTP error, 404 unable to locate website 
-app.use((req, res) => { 
-    res.status(404).send("Error 404! page Not Found");
+//HTTP error, 404 unable to locate website
+app.use((req, res) => {
+  res.status(404).send('Error 404! page Not Found');
 });
