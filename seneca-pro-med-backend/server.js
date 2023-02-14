b9b88ba1fc0aaa1f392ec02c121bef93a2744fa5
Humam Bahoo <humam.bahoo@gmail.com>
2023-02-14 14:11:48 -0500

    add stoppable tool to gracefully shutdown server

--- seneca-pro-med-backend/server.js
+++ seneca-pro-med-backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+// to gracefully shutdown the server
+const stoppable = require('stoppable');
+
 const clientControllers = require('./controllers/client-controller.js');
 const adminControllers = require('./controllers/admin-controller.js');
 const driverControllers = require('./controllers/driver-controller.js');
@@ -29,22 +32,28 @@ app.use('/driver', driverControllers);
 app.use('/pharmacy', pharmaControllers);
 app.use('/product', productControllers);
 
-app.listen(PORT, () => {
-  console.log(`REST API is up and running on PORT: ${PORT}`);
+// start listening on the port. wrapped with stoppable to gracefully shutdown the server
+const server = stoppable(
+  app.listen(PORT, () => {
+    console.log(`REST API is up and running on PORT: ${PORT}`);
 
-  mongoose
-    .connect(
-      `mongodb+srv://senecapromed:senec%40ProMed1234@senecapromeddb.xjhswji.mongodb.net/UsersDB?retryWrites=true&w=majority`
-    )
-    .then(() => {
-      console.log(`Connected to the MongoDB`);
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
-});
+    mongoose
+      .connect(
+        `mongodb+srv://senecapromed:senec%40ProMed1234@senecapromeddb.xjhswji.mongodb.net/UsersDB?retryWrites=true&w=majority`
+      )
+      .then(() => {
+        console.log(`Connected to the MongoDB`);
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      });
+  })
+);
 
 //HTTP error, 404 unable to locate website
 app.use((req, res) => {
   res.status(404).send('Error 404! page Not Found');
 });
+
+// exporting an instance to be accessed by other parts if necessary
+module.exports = server;
