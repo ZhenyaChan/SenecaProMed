f35a20be3d7ffd2ae8bf19ac13e40b8c6029aa1d
kevin <kpsanpedro@myseneca.ca>
2023-03-11 12:22:29 -0500

    feat: add login links

--- seneca-pro-med-front-end/src/components/HeaderComponent.jsx
+++ seneca-pro-med-front-end/src/components/HeaderComponent.jsx
@@ -81,7 +81,7 @@ const HeaderComponent = () => {
                   My Account
                 </p>
               </Link>
-              <Link to={"/"}>
+              <Link to={"/login"}>
                 <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
                   <MdLogin /> Sign in
                 </p>
