a230b461cb630044d7c86efe51d71ea31081cb62
kevin <kpsanpedro@myseneca.ca>
2023-03-27 09:18:34 -0400

    Feat: add admin home page

--- seneca-pro-med-front-end/src/App.js
+++ seneca-pro-med-front-end/src/App.js
@@ -1,12 +1,24 @@
 import ProductsList from "./components/product/ProductList";
+import AdminDashboard  from "./components/admin/AdminHome"
+import AuthContext from './contexts/AuthContext';
+import { useContext } from 'react';
 
 const App = ({ userRole }) => {
   // TODO: validate which user is authenticated and render component accordingly
-
+  const { userData } = useContext(AuthContext);
   const componentToRender = (userRole) => {
     // TODO: add a switch statement here
+    
+    switch (userData.role) {
+      case 'admin':
+        return <AdminDashboard/>;
+      case 'client':
+        return <ProductsList/>;
+      case 'pharmacist':
+        return <ProductsList/>; //update later pharmacis dashboard
+      case 'driver':
+        return <ProductsList/>; //update later for driver dashboard
 
-    switch (userRole) {
       default:
         return <ProductsList />;
     }
--- seneca-pro-med-front-end/src/components/admin/AdminList.jsx
+++ seneca-pro-med-front-end/src/components/admin/AdminList.jsx
@@ -89,7 +89,7 @@ export default function AdminList() {
             type="button"
             className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             onClick={() => {
-              navigate(`/../admin/client/signup`);
+              navigate(`/../admin/signup`);
             }}
           >
             Add User
