7e97acedbb245241d2e43d20db6b1df6ab7d427b
I68439663 <abajin@myseneca.ca>
2023-04-15 13:45:00 -0400

    update: removed order button on client orders page

--- seneca-pro-med-front-end/src/components/client/ClientOrders.jsx
+++ seneca-pro-med-front-end/src/components/client/ClientOrders.jsx
@@ -125,17 +125,6 @@ export default function AdminOrders() {
                   </div>
                </div>
             </div>
-            <div className="flex justify-end">
-               <button
-                  type="button"
-                  className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                  onClick={() => {
-                     navigate(`/../admin/orders`);
-                  }}
-               >
-                  Add Order
-               </button>
-            </div>
             <br />
             <br />
             <br />
